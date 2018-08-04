5847e4218403070ba75f78f3e057f2cd023b91bd
Paul Buonopane <paul@namepros.com>
2018-08-04 02:04:13 -0400

    Phrasing

--- src/App.php
+++ src/App.php
@@ -259,7 +259,7 @@ class App
 		try {
 			// Only notify if there wasn't an exception; otherwise, we've already notified.
 			if ($exception === null) {
-				$this->notify("Bot online.");
+				$this->notify("Bot is online.");
 			} else {
 				// We know the exception has been reported by this point, so clear it.
 				$exception = null;
